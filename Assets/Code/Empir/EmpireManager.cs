808ca31b08330a028c66973221863e2cac31457f
kurama <itumonikoniko365@gmail.com>
2021-02-02 03:32:32 +0900

    帝国軍の生成

---
+++ Assets/Code/Empir/EmpireManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmpireManager : MonoBehaviour
+{
+    // 隊列の発生までの期間
+    [SerializeField] float m_span;
+    // 隊列
+    List<List<BlockParameter>> m_blockParameters;
+    // 帝国軍ジェネレータ
+    EmpirGenerator m_empirGenerator;
+    // カウンター
+    float m_counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_empirGenerator = GetComponent<EmpirGenerator>();
+        m_blockParameters = new List<List<BlockParameter>>();
+        Generate();
+    }
+
+    private void FixedUpdate()
+    {
+        if(m_counter <= 0)
+            Generate();
+
+        m_counter -= Time.fixedDeltaTime;
+    }
+
+    private void Generate()
+    {
+        m_counter = m_span;
+
+        m_blockParameters.Add(m_empirGenerator.Generate());
+
+        foreach (var hor in m_blockParameters)
+        {
+            foreach (var block in hor)
+            {
+                block.moveObject.Move(Vector2Int.down);
+            }
+        }
+    }
+}
